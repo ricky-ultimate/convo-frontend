986997d20f5ca0ca1b8c0a3255087662846e67e0
Ricky Ultimate <rickyogigi@gmail.com>
2025-06-02 23:54:42 +0100

    chore: fix lint issue

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -109,7 +109,7 @@ export default function LoginPage() {
         </form>
 
         <div className="text-center text-sm text-muted-foreground">
-          Don't have an account?{" "}
+          Don&apos;t have an account?{" "}
           <Link href="/auth/register" className="text-primary hover:underline">
             Sign up
           </Link>
